9da10df2640bb1d40b3b24a1203f0d9a4a1c2fa5
thang-260704 <phanhtran0501@gmail.com>
2024-11-19 20:27:42 +0700

    Add files via upload

---
+++ Session2_Lession3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	int a = 10, b = 5;
+	printf("Gia tri cua a la %d\n",a);
+	printf("Gia tri cua b la %d\n",b);
+	int tong = a + b;
+	printf("Tong cua a va b la %d\n",tong);
+	int hieu = a - b;
+	printf("Hieu cua a va b la %d\n",hieu);
+	int tich = a * b;
+	printf("Tich cua a va b la %d\n",tich);
+	int thuong = a / b;
+	printf("Thuong cua a va b la %d",thuong);
+	return 0;
+}
