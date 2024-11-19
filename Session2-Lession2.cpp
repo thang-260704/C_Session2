40f84d16914ec6899dc273fdc00c4de20d77ce2d
thang-260704 <phanhtran0501@gmail.com>
2024-11-19 11:41:11 +0700

    Add files via upload

---
+++ Session2-Lession2.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	int a = 10, b = 5;
+	printf("Gia tri cua a la %d\n",a);
+	printf("Gia tri cua b la %d\n",b);
+	int tong = a + b;
+	printf("Tong cua a va b la %d",tong);
+	return 0;
+}
---
+++ Session2_Lession1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+long long n;// Biến n được khai báo kiểu dữ liệu long long
+int i;//Biến i được khai báo kiểu dữ liệu interger
+int main()
+{
+    char c;//Biến c được khai báo kiểu dữ liệu char
+    printf("Hello, World!");
+}
